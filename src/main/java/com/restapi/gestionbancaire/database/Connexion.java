b83f2a1db0fc63d4059efd82061b2fcbd6bc7f9c
JungleMind <81266435+JungleMind@users.noreply.github.com>
2022-09-05 16:20:19 +0100

    Update Connexion.java

--- src/main/java/com/restapi/gestionbancaire/database/Connexion.java
+++ src/main/java/com/restapi/gestionbancaire/database/Connexion.java
@@ -9,7 +9,7 @@ public class Connexion {
 	    Connection con=null;  
 	    try{  
 	        Class.forName("org.postgresql.Driver");  
-	        //con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/bancaire","postgres","maharo19");  
+	        con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/bancaire","postgres","maharo19");  
 	    }
 	    catch(Exception e){
 	    	System.out.println("DATABASE "+ e);
